af033c488fd3bb75daa95c81a846f520d2d9e3d9
Tiffany Lyssa <tiffanylyssa.p@gmail.com>
2026-07-29 16:15:26 +0800

    removed vue totally
    forgot to delete vue totally when I switched to react

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,7 +1,5 @@
-import inertia from '@inertiajs/vite';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
@@ -17,16 +15,7 @@ export default defineConfig({
                 }),
             ],
         }),
-        inertia(),
         tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
         wayfinder({
             formVariants: true,
         }),
